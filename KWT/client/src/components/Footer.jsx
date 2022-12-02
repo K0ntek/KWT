85df06def444d66ce3781682e128a1176f80b96f
Kuba <93491047+K0ntek@users.noreply.github.com>
2022-12-02 12:34:36 +0100

    Add files via upload

--- KWT/client/src/components/Footer.jsx
+++ KWT/client/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 const Footer = () => {
+
 	return (
 		<div className="footer">
 			<div className="tags">
@@ -12,14 +14,9 @@ const Footer = () => {
 				{/* <img src="" /> */}
 			</div>
 			<div className="border"></div>
-			<div className="socials">
-				<a href="https://www.facebook.com/ZSTiOBANACH">
-					<i className="icon-facebook" />
-				</a>
-				<a href="https://www.instagram.com/zstio.fm/">
-					<i className="icon-instagram icon" />
-				</a>
-			</div>
+			{/* <Wrapper apiKey={"YOUR_API_KEY"} render={render}>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81987.37623194218!2d19.9348336744093!3d50.046854813592944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471644c0354e18d1%3A0xb46bb6b576478abf!2zS3Jha8Ozdw!5e0!3m2!1spl!2spl!4v1669977413818!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</Wrapper> */}
 		</div>
 	);
 };
--- KWT/client/src/pages/Home.js
+++ KWT/client/src/pages/Home.js
@@ -11,28 +11,28 @@ const Home = () => {
 
 
   useEffect(() => {
-    const tl = gsap.timeline({defaults:{ease:"power1.out"}});
-    tl.to(".hiddenText", {y: "0", duration: 1, stagger:0.2})
-    tl.to(".slider", {y: "-100%", duration: 1, delay: 0.5})
-    tl.to(".sliderNote", {y: "-100%", duration: 1, delay: -.7})
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+    tl.to(".hiddenText", { y: "0", duration: 1, stagger: 0.2 })
+    tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 })
+    tl.to(".sliderNote", { y: "-100%", duration: 1, delay: -.7 })
 
-    tl.to(".kwtI", {x: "-250px", duration:0.4, opacity:1})
-    tl.to(".kwtI", {x: "-220px", duration:0.2,})
+    tl.to(".kwtI", { x: "-250px", duration: 0.4, opacity: 1 })
+    tl.to(".kwtI", { x: "-220px", duration: 0.2, })
 
-    tl.to(".kwtIII", {x: "250px", duration:0.4, opacity:1})
-    tl.to(".kwtIII", {x: "220px", duration:0.2,})
+    tl.to(".kwtIII", { x: "250px", duration: 0.4, opacity: 1 })
+    tl.to(".kwtIII", { x: "220px", duration: 0.2, })
   })
 
-  
-  
+
+
   return (
     <>
-    <div className='sliderNote'>
-    <div className='hideText'><p className='hiddenText'>KONKURS</p></div>
-    <div className='hideText'><p className='hiddenText'>WIEDZY</p></div>
-    <div className='hideText'><p className='hiddenText'>TECHNICZNEJ</p></div>
-    </div>
-    <div className='slider'></div>
+      <div className='sliderNote'>
+        <div className='hideText'><p className='hiddenText'>KONKURS</p></div>
+        <div className='hideText'><p className='hiddenText'>WIEDZY</p></div>
+        <div className='hideText'><p className='hiddenText'>TECHNICZNEJ</p></div>
+      </div>
+      <div className='slider'></div>
       <div className="header" id="header">
         <div className="header_elements">
           <div className="kwt">
@@ -53,13 +53,22 @@ const Home = () => {
             </div>
           </div>
         </div>
+
+        <div className="socials">
+          <a href="https://www.facebook.com/ZSTiOBANACH">
+            <i className="icon-facebook" />
+          </a>
+          <a href="https://www.instagram.com/zstio.fm/">
+            <i className="icon-instagram icon" />
+          </a>
+        </div>
       </div>
       <About />
       <HeaderQuestions />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"
-      integrity="sha512-H6cPm97FAsgIKmlBA4s774vqoN24V5gSQL4yBTDOY2su2DeXZVhQPxFK4P6GPdnZqM9fg1G3cMv5wD7e6cFLZQ=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"></script>
+        integrity="sha512-H6cPm97FAsgIKmlBA4s774vqoN24V5gSQL4yBTDOY2su2DeXZVhQPxFK4P6GPdnZqM9fg1G3cMv5wD7e6cFLZQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
     </>
   );
 };
